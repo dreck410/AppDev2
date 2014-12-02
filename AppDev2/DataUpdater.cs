427fe91a9bfb21aebc0244c396bd58bf1d0aaff5
CKost <ckost598@students.bju.edu>
2014-12-02 16:16:23 -0500

    Added Data  method template
    Added example code to fill in

--- AppDev2/DataUpdater.cs
+++ AppDev2/DataUpdater.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Data.SqlClient;
 namespace AppDev2
 {
-    class DataUpdater
+    class DataUpdater  //http://www.codeproject.com/Articles/4416/Beginners-guide-to-accessing-SQL-Server-through-C
     {
+        public static void connect()
+        {
+            /*SqlConnection myConnection = new SqlConnection("user id=username;" +
+                                       "password=password;server=serverurl;" +
+                                       "Trusted_Connection=yes;" +
+                                       "database=database; " +
+                                       "connection timeout=30");
+            try
+            {
+                myConnection.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }*/
+        }
+        
+        
         //blah blah do the good stuff here
     }
 }
