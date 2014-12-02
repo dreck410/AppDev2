6b7c9235403f1830449822d399067f997eb1880c
CKost <ckost598@students.bju.edu>
2014-12-02 15:43:06 -0500

    Made my first change
    For testing

--- AppDev2/Program.cs
+++ AppDev2/Program.cs
@@ -11,6 +11,7 @@ namespace AppDev2
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// this is added by CKost
         [STAThread]
         static void Main()
         {
