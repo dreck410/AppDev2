2946f435c529e736c6f3eeb1135a034b146b175b
CKost <ckost598@students.bju.edu>
2014-12-04 21:53:46 -0500

    Started on SongUsageView....Library closing :(
    Basics layed out

--- AppDev2/dataBaseConnector.cs
+++ AppDev2/dataBaseConnector.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
-
+using System.Data;
 namespace AppDev2
 {
 
@@ -62,6 +62,22 @@ namespace AppDev2
                 leaders.Add(myReader["Name"].ToString());
             }
             return leaders;
+        }
+         
+        public DataTable getPastSongs() //Gets the view data, retursn the DataTable to be loaded into the view
+        {
+            /*Create a SQL Server view named SongUsageView that displays all of the colums in the Song table,plus one named LastUsedDate. 
+             * The LastUsedDate column should contain the date of the most recent service that used that song. 
+             * (Exclude choral songs, but be sure to include songs which have never been used).
+             * Using this view, display 20 of the least recently used songs, ordered by LastUsedDate, and then song title. 
+             * Allow the user to select songs from this list, and assign them to the congregational song events. (10 points)*/
+            DataTable Songs = new DataTable();
+            using (var command = new SqlCommand("SELECT * FROM Songs", myConnection)) //Confirm name of database
+            {
+                // Loads the query results into the table
+                Songs.Load(command.ExecuteReader());
+            }
+            return Songs;
         }
 
         public int getServiceID(string templateService){
